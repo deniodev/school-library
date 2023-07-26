2cb1d0a2f9f432f2a592cc45e12b0dfb644bb950
Denio Nhanale <dnhanale@gmail.com>
2023-07-26 11:38:13 +0200

    Create a class Rental

---
+++ rental.rb
@@ -0,0 +1,19 @@
+class Rental
+  attr_accessor :date, :book, :person
+
+  def initialize(date, book, person)
+    @date = date
+    assign_book(book)
+    assign_person(person)
+  end
+
+  def assign_book(book)
+    @book = book
+    book.rentals << self
+  end
+
+  def assign_person(person)
+    @person = person
+    person.rentals << self
+  end
+end
